10ad4805c1914fa0c6238fb02dff4d58aa989190
Shilpi Agrawal <shilpi.agrawal2011@vit.ac.in>
2014-04-29 10:30:33 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+
+<frameset rows="6%,10%,46%,*" frameborder="0">
+
+  <frame src="dropdown.html"  scrolling="no">
+<frame src="text1.php"  scrolling="no">
+  <frame src="image-gallery.source.php"  scrolling="no">
+
+  
+ <frame src="c.php"  target="_blank" scrolling="no">
+</frameset>
+
+</html>
+

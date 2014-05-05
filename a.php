d13c12f0c508d6e1d0c73dea9d5fc6c0d05c0cd2
Shilpi Agrawal <shilpi.agrawal2011@vit.ac.in>
2014-05-05 23:40:03 +0530

    Create a.php

---
+++ a.php
@@ -0,0 +1,37 @@
+<html>
+<style type="text/css">
+.cod {
+	
+	background-color:#79bbff;
+	text-indent:0;
+	border:1px solid #84bbf3;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:25px;
+	font-weight:bold;
+	font-style:normal;
+	height:70px;
+	line-height:100px;
+	width:260px;
+	text-decoration:none;
+	text-align:center;
+}.cod:hover {
+	background-color:#378de5;
+}.cod:active {
+	position:relative;
+	top:1px;
+}
+.f1{
+margin-top:100px;
+}</style>
+<body background ="bg1.jpg">
+<form action="cod.php" class="f1" target="bb">
+<input type="submit" class="cod" value="Cash On Delivery"></form>
+<form action="debit.php"target="bb">
+<input type="submit" class="cod" value="Debit Card"></form>
+<form action="credit.php" target="bb">
+<input type="submit" class="cod" value="Credit Card"></form>
+<form action="bank.php" target="bb">
+<input type="submit" class="cod" value="Net Banking"></form>
+
+</html>

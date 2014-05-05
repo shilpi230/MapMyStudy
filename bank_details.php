d777e92ad727ea5994d4b185be3c316031d644c9
Shilpi Agrawal <shilpi.agrawal2011@vit.ac.in>
2014-05-05 23:45:03 +0530

    Create bank_details.php

---
+++ bank_details.php
@@ -0,0 +1,14 @@
+<body>
+<p>Enter your bank details</p>
+<form action="final_1.php">
+<label>Name of the bank</label>
+<input type ="text" name="name"><br><br>
+<label>Branch Name </label>
+<input type="text" name="card"><br><br>
+<label>Branch Code</label>
+<input type="number" name="cvv"><br><br>
+<label>Security Code</label>
+<input type="number" name="cvv"><br><br>
+<input type="submit" value="SAVE AND CONTINUE" name="cd">
+</form>
+</body>

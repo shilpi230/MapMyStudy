50055604852669b8843832d44bcf6f6e75e2dce9
Shilpi Agrawal <shilpi.agrawal2011@vit.ac.in>
2014-05-05 23:23:46 +0530

    Create c.php

---
+++ c.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.thumbnail 
+{
+float:left;
+width:110px;
+height:90px;
+margin:5px;-webkit-box-shadow: 5px 5px 5px #111;
+          box-shadow: 5px 5px 5px #111;  
+  -webkit-transition: all 1s ease;
+     -moz-transition: all 1s ease;
+       -o-transition: all 1s ease;
+      -ms-transition: all 1s ease;
+          transition: all 1s ease;
+
+}
+ 
+.thumbnail:hover {
+  width: 150px;
+  height: 100px;
+
+}
+}
+</style>
+</head>
+<body>
+<h3 style="margin-left:200px;">Image Gallery</h3>
+<div style="margin-left:200px;">
+<a href="" target="_self"  ><img class="thumbnail" src="images/books_8.jpg" width="107" height="90" ></a>
+<a href="" target="_self"   ><img class="thumbnail" src="images/books_1.jpg" width="107" height="80"></a>
+<a href="" target="_self"  ><img class="thumbnail" src="images/books_2.jpg" width="116" height="90"></a>
+<a href="" target="_self"  ><img class="thumbnail" src="images/books_3.jpg" width="120" height="90"></a>
+<a href="" target="_self"  ><img class="thumbnail" src="images/books_4.jpg" width="107" height="90"></a>
+<a href="" target="_self"  ><img class="thumbnail" src="images/books_5.jpg" width="107" height="80"></a>
+<a href="" target="_self" ><img class="thumbnail" src="images/books_6.jpg" width="116" height="90"></a>
+<a href="" target="_self"  ><img class="thumbnail" src="images/books_7.jpg" width="120" height="90"></a>
+</div></body>
+</html>

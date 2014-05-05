7a3bc4ab2adf13030bd8a9c394ac3e4fa7023fb9
Shilpi Agrawal <shilpi.agrawal2011@vit.ac.in>
2014-05-05 23:54:51 +0530

    Create final_1.php

---
+++ final_1.php
@@ -0,0 +1,7 @@
+<?
+include("value_extract.php");
+echo "Total amount:" .$s. "<br>";
+$sql="delete from book_id";
+$res=mysql_query($sql,$conn);
+echo "<h2>Thank you for shopping with us !!!! </h2>";
+?>

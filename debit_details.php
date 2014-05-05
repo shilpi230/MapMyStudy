952bc8361d0cd001c34f0b279c0ae9e99523446a
Shilpi Agrawal <shilpi.agrawal2011@vit.ac.in>
2014-05-05 23:52:10 +0530

    Create debit_details.php

---
+++ debit_details.php
@@ -0,0 +1,30 @@
+<head>
+<style>
+h1{
+margin-top:70px;
+color:Aquamarine;
+}
+</style>
+</head>
+<body>
+<p>Enter your card details</p>
+<form action="final_1.php">
+<label> Name on the card:</label>
+<input type ="text" name="name"><br><br>
+<label> Card Number: </label>
+<input type="number" name="card"><br><br>
+<label>CVV:</label>
+<input type="number" name="cvv"><br><br>
+<label>Card Type</label>
+<select>
+<option>Master Card</option>
+<option>Mastero Card</option>
+<option>American Express</option>
+<option>Visa Card</option>
+</select><br><br>
+<label>PIN:</select>
+<input type="number" name="pin"><br><br>
+<input type="submit" value="SAVE AND CONTINUE" name="cd">
+</form>
+</body>
+

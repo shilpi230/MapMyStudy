10fb11b040744d4b86a6d80834706ea5039ad718
Shilpi Agrawal <shilpi.agrawal2011@vit.ac.in>
2014-05-05 23:51:20 +0530

    Create credit.php

---
+++ credit.php
@@ -0,0 +1,23 @@
+<?php
+include("value_extract.php");
+?>
+<head>
+<style>
+h1{
+margin-top:70px;
+color:Aquamarine;
+}
+</style>
+</head>
+<body>
+<h1>Credit Card</h1>
+<p>Enter your shipping address</p>
+<form action="card_details.php" method="POST">
+<label>Name</label><input type="text" name="name"><br><br>
+<label>Pincode</label><input type="text" name="pincode"><br><br>
+<label>Landmark</label><input type="text" name="address"><br><br>
+<label>Address</label><textarea name="landmark"></textarea><br><br>
+<label>Phone</label><input type="text" name="phone"><br><br>
+<input type="submit" value="SAVE AND CONTINUE" name="cod">
+</form>
+</body>
